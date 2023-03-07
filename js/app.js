5b3085ff061fa8a5ec13da07ec29be5d2eed4fe3
Scott Jones <itspinky95@hotmail.com>
2023-03-07 10:05:25 +0000

    Building Hunter game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+let game;
+
+document.getElementById('btn__reset').addEventListener('click', (e) => {
+    game = new Game();
+    game.startGame();
+
+});
+
+document.getElementById('qwerty').addEventListener('click', (e) => {
+    if ( e.target.tagName === 'BUTTON' ) {
+         game.handleInteraction(e.target);
+    };
+});
+
+document.addEventListener('keyup', (e) => {
+    const keys = document.getElementsByClassName('key');
+    for (let i = 0; i < keys.length; i++){
+        if (keys[i].innerText === e.key){
+            game.handleInteraction(keys[i]);
+        }
+    }
+});
