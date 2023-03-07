0494cb883f97a9b187dcfdda570b0c8bc8b3341c
Scott Jones <itspinky95@hotmail.com>
2023-03-07 10:06:44 +0000

    Fininshing building game

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,118 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor () {
+        this.missed = 0;
+        this.phrases = [new Phrase('there is no place like home'),
+                        new Phrase('just keep swimming'),
+                        new Phrase('there is no crying in baseball'),
+                        new Phrase('to infinity and beyond'),
+                        new Phrase('you are killing me smalls')
+                    ];
+        this.activePhrase = null;
+    }
+
+    startGame() {
+        document.getElementById('overlay').style.display = 'none';
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    getRandomPhrase() {
+        return this.phrases[Math.floor(Math.random() * this.phrases.length)];
+    }
+
+    checkForWin() {
+        const letters = document.getElementsByClassName('letter');
+        const show = document.getElementsByClassName('show');
+        
+        if(letters.length === show.length) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    removeLife() {
+        const tries = document.querySelectorAll('.tries img');
+
+        for (let i = 0; i < tries.length; i++){
+            tries[this.missed].setAttribute('src', 'images/lostHeart.png');
+        }
+
+        this.missed++;
+
+        if (this.missed === 5) {
+            this.gameOver();
+            this.restartGame();
+        }
+    };
+
+    gameOver() {
+        const overlay = document.getElementById('overlay');
+        overlay.style.display = '';
+
+        const message = document.getElementById('game-over-message');
+        const button = document.getElementById('btn__reset');
+        button.innerText = 'Play Again';
+
+        if ( this.missed < 5 ){
+            message.innerText = 'You Win!';
+            overlay.className = 'win';
+            document.querySelector('.win').style.backgroundColor = '#ff534a';
+        } else {
+            message.innerText = 'Better luck next time!';
+            overlay.className = 'lose';
+            document.querySelector('.lose').style.backgroundColor = '#989e8b';
+
+        }
+
+    };
+
+    handleInteraction(button) {
+        button.disabled = true;
+        
+        if ( this.activePhrase.checkLetter(button.innerHTML) ) {
+            button.classList.add('chosen');
+            this.activePhrase.showMatchedLetter(button.innerHTML);
+
+            if ( this.checkForWin() ) {
+                this.gameOver();
+                this.restartGame();
+            } 
+
+        } else {
+            button.classList.add('wrong');
+            this.removeLife();
+        };
+        
+    }
+
+    restartGame() {
+        this.missed = 0;
+
+        const phrase = document.querySelector('#phrase ul');
+        const items = document.querySelectorAll('#phrase li')
+    
+        const keyboard = document.querySelectorAll('#qwerty button');
+    
+        const tries = document.querySelectorAll('.tries img');
+    
+        for (let i = 0; i < items.length; i++) {
+            phrase.removeChild(items[i]);
+        }
+    
+        for (let i = 0; i < keyboard.length; i++) {
+             keyboard[i].disabled = false;
+            keyboard[i].className = 'key';
+        }
+    
+        for (let i = 0; i < tries.length; i++){
+            tries[i].setAttribute('src', 'images/liveHeart.png');
+        }
+    }
+
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,50 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor (phrase) {
+        this.phrase = phrase;
+    }
+        /**
+         * Display phrase on game board
+         */
+    addPhraseToDisplay() {
+        this.phrase.split('').forEach( character => {
+            const phraseSection = document.querySelector('#phrase ul');
+            const listItem = document.createElement('li');
+            listItem.innerText = character;
+            phraseSection.appendChild(listItem);
+            if ( character === ' ') {
+                listItem.className = 'hide space';
+            } else {
+                listItem.className = `hide letter ${character}`;
+            }
+        });
+    };
+
+        /**
+        * Checks if passed letter is in phrase
+        * @param (string) letter - Letter to check
+        */
+     checkLetter(letter) {
+        return this.phrase.includes(letter);
+    };
+
+        /**
+        * Displays passed letter on screen after a match is found
+        * @param (string) letter - Letter to display
+        */
+    showMatchedLetter(letter) {
+        const phraseItems = document.querySelectorAll('#phrase li');
+
+        for (let i = 0; i < phraseItems.length; i++){
+            if (letter === phraseItems[i].innerHTML) {
+                phraseItems[i].classList.remove('hide');
+                phraseItems[i].classList.add('show');
+            }
+        };
+        
+    };
+                
+    }
+    
